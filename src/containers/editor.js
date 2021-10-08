53f29a771e5c1b2878d2aa79726b3b81f3091b20
liguohang <liguohang@huice.com>
2021-10-08 17:41:35 +0800

    check

---
+++ src/containers/editor.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+// import {createChangeParaModeAction} from '../redux/actions/paragraph'
+
+class Editor extends Component {
+
+    render() {
+        const views = this.props.fileContent.map((current,index)=>{
+            let res
+            switch (current.type){
+                case 'h1':
+                    res = <h1 key={current.id} suppressContentEditableWarning='true' contentEditable='true'>{current.content}</h1>
+                    break
+                default:
+                    res = <p suppressContentEditableWarning='true' contentEditable='true'></p>
+                    break
+            }
+            return res
+        })
+        return (
+            <div>
+                {views}
+            </div>
+            
+        )
+    }
+    
+}
+
+export default connect(
+    state=>({
+        fileContent:state.fileContent,
+    }),
+    {
+        // changeMode:createChangeParaModeAction
+    }
+)(Editor)
